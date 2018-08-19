2a86b3bbba73120600abca81f6a03341a99c1ec5
indiflex <indiflex1@gmail.com>
2018-08-19 16:46:03 +0900

    maven compile 1.8

--- src/main/java/com/jade/swp/persistence/BoardDAO.java
+++ src/main/java/com/jade/swp/persistence/BoardDAO.java
@@ -5,15 +5,15 @@ import java.util.List;
 import com.jade.swp.domain.Board;
 
 public interface BoardDAO {
-	public void create(Board board) throws Exception;
+	void create(Board board) throws Exception;
 
-	public Board read(Integer bno) throws Exception;
+	Board read(Integer bno) throws Exception;
 
-	public void update(Board board) throws Exception;
+	void update(Board board) throws Exception;
 
-	public void delete(Integer bno) throws Exception;
+	void delete(Integer bno) throws Exception;
 
-	public List<Board> listAll() throws Exception;
+	List<Board> listAll() throws Exception;
 
-	public Integer getMaxbno() throws Exception;
+	Integer getMaxbno() throws Exception;
 }
