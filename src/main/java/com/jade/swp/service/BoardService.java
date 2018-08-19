bdb842f6cdcf3778b64c1fba53090084b42a6699
indiflex <indiflex1@gmail.com>
2018-08-19 16:00:48 +0900

    maven compile 1.8

--- src/main/java/com/jade/swp/service/BoardService.java
+++ src/main/java/com/jade/swp/service/BoardService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.jade.swp.domain.Board;
 
 public interface BoardService {
+	
 	public void regist(Board board) throws Exception;
 
 	public Board read(Integer bno) throws Exception;
